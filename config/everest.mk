7ead2d2074e0db4abf26e49dcc18c0396d40f9cc
aswin7469 <aswinas@pixysos.com>
2023-11-18 10:43:29 +0530

    evolution: Enable Cloned App
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- config/everest.mk
+++ config/everest.mk
@@ -1,2 +1,6 @@
 # Bootanimation
 include vendor/everest/common/config/bootanimation.mk
+
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/everest/common/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-everest-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-everest-product.xml
