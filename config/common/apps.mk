f342ae6ad96bf86d13ca3f762b0341f2648052bb
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-12-25 18:32:17 +0900

    fixup! [TMP] Don't buid some apps

--- config/common/apps.mk
+++ config/common/apps.mk
@@ -3,8 +3,8 @@ PRODUCT_PACKAGES += \
     BatteryStatsViewer
 
 # BtHelper
-# PRODUCT_PACKAGES += \
-#     BtHelper
+PRODUCT_PACKAGES += \
+    BtHelper
 
 ifneq ($(MAINLINE_INCLUDE_BTSERVICES_MODULE),false)
 # Ship custom libbluetooth_jni to system partition
@@ -29,24 +29,24 @@ PRODUCT_PACKAGES += \
     SystemUIFlagFlipper
 
 # TouchGestures
-# PRODUCT_PACKAGES += \
-#     TouchGestures
+PRODUCT_PACKAGES += \
+    TouchGestures
 
 # Face Unlock
-# ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
-# TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
 
-# PRODUCT_PACKAGES += \
-#     AOSPASettingsOverlay
+PRODUCT_PACKAGES += \
+    AOSPASettingsOverlay
 
-# ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
-# PRODUCT_PACKAGES += \
-#     ParanoidSense
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    ParanoidSense
 
-# PRODUCT_SYSTEM_PROPERTIES += \
-#     ro.face.sense_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.face.sense_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
 
-# PRODUCT_COPY_FILES += \
-#     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
-# endif
-# endif
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+endif
