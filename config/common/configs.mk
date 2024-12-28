2a2a74ab35d002b23a57156a06a449c0bb285356
janak <niraulajanak2019@gmail.com>
2024-12-28 14:31:15 +0545

    vendor: Everestify

--- config/common/configs.mk
+++ config/common/configs.mk
@@ -1,6 +1,6 @@
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(VENDOR_PARASITE_COMMON_DIR)/misc/keylayout/Vendor_2dc8_Product_6006.kl:$(TARGET_COPY_OUT_PRODUCT)/usr/keylayout/Vendor_2dc8_Product_6006.kl
+    $(VENDOR_EVEREST_COMMON_DIR)/misc/keylayout/Vendor_2dc8_Product_6006.kl:$(TARGET_COPY_OUT_PRODUCT)/usr/keylayout/Vendor_2dc8_Product_6006.kl
 
 ifneq ($(PRODUCT_TYPE),go)
 # Freeform window management
