7fc95f5be92639c9968e160afe0c449bec3b60c1
janakniraula <janakniraula@users.noreply.github.com>
2025-02-15 06:51:22 +0000

    everest: Inherit Themed Icons

--- config/everest.mk
+++ config/everest.mk
@@ -4,3 +4,6 @@ include vendor/everest/common/config/bootanimation.mk
 # Cloned app exemption
 PRODUCT_COPY_FILES += \
     vendor/everest/common/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-everest-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-everest-product.xml
+
+# Themed icons for Pixel Launcher
+$(call inherit-product, vendor/google/overlays/ThemeIcons/config.mk)
